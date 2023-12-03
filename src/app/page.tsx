07188727670e8109c7994a6797d67ac8c094faa2
João Lee <joaumlimaum@gmail.com>
2023-12-02 23:31:02 -0300

    Created header - links and sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import HomeHeader from "@/components/HomeHeader";
+
 export default function Home() {
   return (
-    <div className="text-red-400">Home</div>
+    <>
+    <HomeHeader />
+    </>
   )
 }
