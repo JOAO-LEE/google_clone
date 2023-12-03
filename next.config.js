533bd023f92f531aaa5c6bcef10a86e18624f647
João Lee <joaumlimaum@gmail.com>
2023-12-03 02:42:49 -0300

    Created search bar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            protocol: "https",
+            port: "",
+            hostname:  "www.google.com.br",
+            pathname: "/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
+      
+          }]
+    }
+}
 
 module.exports = nextConfig
